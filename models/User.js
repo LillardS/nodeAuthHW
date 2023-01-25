211225c218a9fee70b0125ce8dfff3534e8febaf
Seth Lillard <lillardseth@gmail.com>
2023-01-24 20:05:21 -0500

    update

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -23,8 +24,9 @@ userSchema.post('save', function (doc, next) {
 });
 
 // fire a function before doc saved to db
-userSchema.pre('save', function (next) {
-  console.log('user about to be created & saved', this);
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
