6e0f36a7ab53dd437ce0e3acbc5e3567a76fb6e1
Seth Lillard <lillardseth@gmail.com>
2023-02-06 14:29:49 -0500

    update

--- app.js
+++ app.js
@@ -2,26 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
 
 const app = express();
 
 // middleware
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
 // view engine
+
 app.set('view engine', 'ejs');
 
 // database connection
+
 const dbURI = 'mongodb+srv://Seth:Hi1234@nodeauth.ipzy4u3.mongodb.net/?retryWrites=true&w=majority';
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
+
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 // // cookies
